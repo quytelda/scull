02fd9252fe6a29934a8e27211dee5d4a6f3ce8f1
Quytelda Kahja <quytelda@tamalin.org>
2018-06-13 23:21:52 -0700

    Fix licensing and copyrights.

--- scull.c
+++ scull.c
@@ -1,3 +1,19 @@
+/*
+ * main.c -- the bare scull char module
+ *
+ * Copyright (C) 2001 Alessandro Rubini and Jonathan Corbet
+ * Copyright (C) 2001 O'Reilly & Associates
+ *
+ * The source code in this file can be freely used, adapted,
+ * and redistributed in source or binary form, so long as an
+ * acknowledgment appears in derived source files.  The citation
+ * should list that the code comes from the book "Linux Device
+ * Drivers" by Alessandro Rubini and Jonathan Corbet, published
+ * by O'Reilly & Associates.   No warranty is attached;
+ * we cannot take responsibility for errors or fitness for use.
+ *
+ */
+
 #include <linux/fs.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -10,6 +26,9 @@ static int scull_nr_devs = SCULL_NR_DEVS;
 
 module_param(scull_major, int, 0644);
 module_param(scull_minor, int, 0644);
+
+MODULE_AUTHOR("Alessandro Rubini, Jonathan Corbet");
+MODULE_LICENSE("Dual BSD/GPL");
 
 static int __init scull_init(void)
 {
--- scull.h
+++ scull.h
@@ -1,3 +1,20 @@
+/*
+ * scull.h -- definitions for the char module
+ *
+ * Copyright (C) 2001 Alessandro Rubini and Jonathan Corbet
+ * Copyright (C) 2001 O'Reilly & Associates
+ *
+ * The source code in this file can be freely used, adapted,
+ * and redistributed in source or binary form, so long as an
+ * acknowledgment appears in derived source files.  The citation
+ * should list that the code comes from the book "Linux Device
+ * Drivers" by Alessandro Rubini and Jonathan Corbet, published
+ * by O'Reilly & Associates.   No warranty is attached;
+ * we cannot take responsibility for errors or fitness for use.
+ *
+ * $Id: scull.h,v 1.15 2004/11/04 17:51:18 rubini Exp $
+ */
+
 #ifndef _SCULL_H_
 #define _SCULL_H_
 
